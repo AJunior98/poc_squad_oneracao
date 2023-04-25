f70622d9b5a6da1e900109bd5966c3c333fa7446
Amauri Junior <100853329+AJunior98@users.noreply.github.com>
2023-04-25 17:05:22 -0300

    Issue#2 (#9)
    * Ajustes no microserviço de cotação para utilizar DynamoDB
    
    * Ajuste em alguns arquivos
    
    * Implementação concluida sem testes
    
    * Ajustes e documentação de alguns métodos
    
    * Update README.md
    
    * Update README.md

--- cotacao/src/main/java/org/br/agro/infra/message/KafkaEvents.java
+++ cotacao/src/main/java/org/br/agro/infra/message/KafkaEvents.java
@@ -16,6 +16,9 @@ public class KafkaEvents {
     @Channel("quotation-channel")
     Emitter<QuotationDTO> quotationRequestEmitter;
 
+    //O método "toCompletableFuture()" serve obter um CompletableFuture que será concluído quando a operação de envio da mensagem for concluída.
+    //O método "join()" do CompletableFuture serve para bloquear a execução do método até que a operação de envio da mensagem seja concluída.
+    //Isso garante que o método só retorne após a mensagem ter sido enviada com sucesso para o tópico Kafka.
     public void sendNewKafkaEvent(QuotationDTO quotation){
         LOG.info("-- Enviando Cotação para Tópico Kafka --");
         quotationRequestEmitter.send(quotation).toCompletableFuture().join();
